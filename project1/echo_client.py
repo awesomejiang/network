196352791c53bea7ce6e87587e7fdcfad27c1398
awesomejiang <awesomejiang1995@gmail.com>
2018-01-10 18:39:19 -0600

    first project about echo system

---
+++ project1/echo_client.py
@@ -0,0 +1,29 @@
+import sys
+import socket
+
+#read input arguments
+hostname = sys.argv[1]
+port = int(sys.argv[2])
+
+def main():
+	#infinite loop
+	while 1:
+		#create a client socket
+		clientsocket = socket.socket(
+			socket.AF_INET, socket.SOCK_STREAM)
+
+		#connect to server
+		clientsocket.connect((hostname, port))
+		#send text to server
+		print "Please input the text you want to sent:"
+		text = raw_input()
+		clientsocket.send(text)
+		#output echo from server
+		echo_text = clientsocket.recv(512)
+		print "Received text from server:"
+		print echo_text+'\n'
+		#disconnect
+		clientsocket.close()
+
+if __name__ == '__main__':
+	main()
